1602d44738b9d978df81e1e1e13c6de165832997
Viktorov Vladimir <vladimirv@123software.ru>
2017-10-12 17:29:57 +0300

    buttons

--- calculator/CalculatorViewController.swift
+++ calculator/CalculatorViewController.swift
@@ -12,7 +12,12 @@ class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
-    var display
+    var displayNumber : Double = 0
+    var operand1 : String = "";
+    var operand2 : String = "";
+    var operation : String = "";
+    
+    var userTyping = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +30,41 @@ class CalculatorViewController: UIViewController {
     }
 
     @IBAction func digitBtnTouchUpInside(_ sender: UIButton) {
+        let digit = sender.currentTitle
+        if userTyping
+        {
+            displayLabel.text?.append(digit!)
+        }
+        else
+        {
+            displayLabel.text = digit
+            userTyping = true
+        }
     }
     
+    @IBAction func operationBtnTap(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        switch operation {
+        case "C":
+            displayLabel.text = "0"
+            userTyping = false
+            operand1 = ""
+            operand2 = ""
+        case ".":
+            displayLabel.text?.append(".")
+        case "=":
+            operand2 = displayLabel.text!
+            
+            displayLabel.text = "\(operand1) \(self.operation) \(operand2)"
+            userTyping = false
+        case "+", "-", "*", "/":
+            operand1 = displayLabel.text!
+            self.operation = operation
+        case "\u{221A}":
+            operand1 = displayLabel.text!
+        default:
+            <#code#>
+        }
+    }
 }
 
